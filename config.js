2c142df418df9ebfc697d842057ce9fdd94ffc24
ysnsunsisjsisjsisms <108570692+ysnsunsisjsisjsisms@users.noreply.github.com>
2022-10-30 19:36:01 +0100

    Update config.js

--- config.js
+++ config.js
@@ -18,8 +18,8 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','62895336722950','6287721317870','6288292024190']
-global.premium = ['6288292024190']
+global.owner = ['393516588957']
+global.premium = ['393516588957']
 global.packname = 'Hisoka Morrow'
 global.author = 'WhatsApp Bot'
 global.sessionName = 'hisoka'
